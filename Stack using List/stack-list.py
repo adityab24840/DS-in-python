93e7fc886fe5d2959ec75d49c33b20e458fba1f8
Aditya N Bhatt <56029648+adityab24840@users.noreply.github.com>
2021-05-08 21:00:52 +0530

    Create stack-list.py

---
+++ Stack using List/stack-list.py
@@ -0,0 +1,24 @@
+#using list
+stack=[]
+
+stack.append('1')
+stack.append('2')
+stack.append('3')
+
+print("stack before popping")
+print(stack)
+
+print(stack.pop())
+print("satck after popping 1st element")
+print(stack)
+
+print(stack.pop())
+print("satck after popping 2nd element")
+print(stack)
+
+print(stack.pop())
+print("satck after popping 3rd element")
+print(stack)
+
+print(stack.pop())
+print(stack)
